54278a338b760ccee0139d5818c679e04d2223b5
Jeko0 <japulava@unisens.ge>
2022-10-19 21:35:12 +0400

    added validations for deposit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
+  has_many :products, foreign_key: "seller_id", dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :user_name, presence: true, uniqueness: true
+  validates :deposit, presence: true, 
 
   enum role: %i[ buyer seller ]
   after_create :set_user_role 
 
-  has_many :products, foreign_key: "seller_id", dependent: :destroy
+  
 
   def set_user_role
     self.role ||= :buyer
