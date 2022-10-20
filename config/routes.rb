45fd7ef8fb0eee9e0d243c600a39f0efe4a9c8fe
Jeko0 <japulava@unisens.ge>
2022-10-20 22:05:31 +0400

    added deposit routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,18 @@ Rails.application.routes.draw do
       delete '/', to: "registrations#destroy"
       post '/login', to: "sessions#create"
     end
+
+    #product routes
     get '/products', to: "products#index"
     get '/products/:product_id', to: "products#show"
     post '/products', to: "products#create"
     patch '/products/:product_id', to: "products#update"
     delete '/products/:product_id', to: "products#destroy"
+
+    #deposit routes 
+    get '/deposit', to: "deposits#show"
+    patch '/deposit', to: "deposits#update" 
+    delete '/reset', to: "deposits#destroy"
+    post '/buy', to: "deposits#buy"
   end
 end
