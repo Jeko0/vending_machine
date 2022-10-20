8a19ec1cc471332875cfdf5925a5ae01f31e8b95
Jeko0 <japulava@unisens.ge>
2022-10-20 22:32:13 +0400

    added validation to user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :user_name, presence: true, uniqueness: true
-  validates :deposit, presence: true
+  validates :user_name, presence: true, allow_blank: false, uniqueness: true
+  validates :deposit, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   enum role: %i[ buyer seller ]
   after_create :set_user_role 
-
-  
 
   def set_user_role
     self.role ||= :buyer
